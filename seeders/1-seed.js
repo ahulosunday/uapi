94ba3a505ecab493288adc0695bb59df8b2f0d4c
Ahulo Sunday <ahulosunday@yahoo.com>
2025-08-04 13:07:43 +0100

    Update 1-seed.js

--- seeders/1-seed.js
+++ seeders/1-seed.js
@@ -3,10 +3,10 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-   
+   /*
      await queryInterface.sequelize.query(`SET PERSIST sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
      `);
-  
+  */
   },
 
   async down (queryInterface, Sequelize) {
